7260f400da8ca08e3d68e85459c4edce28f7f4ff
Nick Randall <nick.randall@domo.com>
2015-09-24 20:17:37 -0600

    fixing things

---
+++ platforms/ios/Councils-watch Extension/TableRow.swift
@@ -0,0 +1,14 @@
+//
+//  TableRow.swift
+//  Councils
+//
+//  Created by Nick S Randall on 9/24/15.
+//
+//
+
+import Foundation
+import WatchKit
+
+class TableRow : NSObject {
+    @IBOutlet weak var labelUpdate: WKInterfaceLabel!
+}
